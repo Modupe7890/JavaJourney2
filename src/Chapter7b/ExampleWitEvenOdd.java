1d59fce4b20844f437592e05c8310f951454f8e9
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-05-23 12:36:04 +0530

    Your message

--- src/Chapter7b/ExampleWitEvenOdd.java
+++ src/Chapter7b/ExampleWitEvenOdd.java
@@ -1,7 +1,27 @@
 package Chapter7b;
 
-public class ExampleWitEvenOdd {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+public class ExampleWitEvenOdd {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(true){
+            String s = reader.readLine();
+            if (s.isEmpty()) break;
+
+            int x = Integer.parseInt(s);
+            if(x % 2 == 0) {
+                list.add(x);
+            }
+            else{
+                list.add(0);
+            }
+        }
     }
 }
