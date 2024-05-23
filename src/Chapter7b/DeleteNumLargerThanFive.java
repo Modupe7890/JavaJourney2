68ea599cd8b3db36bf79e151c4428f2d0720775b
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-05-23 16:11:48 +0530

    Your message

--- src/Chapter7b/DeleteNumLargerThanFive.java
+++ src/Chapter7b/DeleteNumLargerThanFive.java
@@ -24,5 +24,6 @@ public class DeleteNumLargerThanFive {
                 i++;
             }
         }
+        System.out.println(list);
     }
 }
---
+++ src/Chapter7b/EvenOddArray.java
@@ -0,0 +1,33 @@
+package Chapter7b;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class EvenOddArray {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] data = {1, 5, 6, 11, 3, 15, 7, 8};
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < data.length; i++) {
+            list.add(data[i]);
+        }
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            Integer x = list.get(i);
+            if (x % 2 == 0) {    // If x is even
+                even.add(x);   // Add x to the collection of even numbers
+
+            } else {
+                odd.add(x);    // Add x to the collection of odd numbers
+                System.out.println(list);
+            }
+        }
+    }
+}
