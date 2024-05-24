34bc885bcae8aa6e99de03e8393d3080aaabd206
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-05-24 15:36:10 +0530

    Your message

--- src/Chapter7b/EvenOddArray.java
+++ src/Chapter7b/EvenOddArray.java
@@ -35,6 +35,7 @@ public class EvenOddArray {
         for(int i  =0; i < odd.size(); i++){
             System.out.println("The odd numbers in the list are :: ::: :: " +odd.get(i));
             System.out.println("The odd numbers in the list are :: ::: :: " +odd.get(i));
+            System.out.println("The odd numbers in the list are :: ::: :: " +odd.get(i));
         }
 
 
