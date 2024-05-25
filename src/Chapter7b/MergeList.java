7805479dcd26853cebce485f24b6ae84c6579d55
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-05-25 14:48:59 +0530

    Your message

---
+++ src/Chapter7b/MergeList.java
@@ -0,0 +1,13 @@
+package Chapter7b;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MergeList {
+    public static void main(String[] args) throws IOException {
+
+        ArrayList<Integer> list1 = new ArrayList<>(); //create a list
+        collections.addAll = (list1,1, 5, 6, 11, 3, 15, 7, 8); //fill a list
+
+    }
+}
