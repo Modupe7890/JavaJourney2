6dd7b754b1013bd27e9f199aa70b473a443c3b43
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-06-08 17:24:14 +0530

    Your message

--- src/Chapter7b/TaskReverse.java
+++ src/Chapter7b/TaskReverse.java
@@ -22,5 +22,7 @@ public class TaskReverse {
         System.out.println(Arrays.toString(numbers));
         System.out.println(Arrays.toString(numbers));
         System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(numbers));
     }
 }
