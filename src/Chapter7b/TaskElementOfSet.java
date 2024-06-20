6fd9101b5cc1a41c9be3c726b8eeca751f6983bf
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-06-20 09:15:59 +0530

    Your message

---
+++ src/Chapter7b/TaskElementOfSet.java
@@ -0,0 +1,22 @@
+package Chapter7b;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class TaskElementOfSet {
+    public static void main(String[] args) {
+
+        Set<String> set = new HashSet<>();
+        set.add("Stop");
+        set.add("Looking,");
+        set.add("Listen");
+
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+
+            String text = iterator.next();
+            System.out.println(text);
+        }
+    }
+}
