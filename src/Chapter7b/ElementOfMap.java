6a50576ec793c7e941e06c3e3ed9a911a3b3f00f
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-06-20 09:58:20 +0530

    Your message

---
+++ src/Chapter7b/ElementOfMap.java
@@ -0,0 +1,24 @@
+package Chapter7b;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class ElementOfMap {
+    public static void main(String[] args) {
+        //All elements are stored in pairs
+
+        Map<String, String> map = new HashMap<>();
+        map.put("first", "Rain");
+        map.put("second", "In");
+        map.put("third", "Spain");
+
+        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()){
+
+            // Get a key-value pair
+            Map.Entry<String, String> pair = iterator.next();
+            String key = pair.getKey();
+        }
+    }
+}
