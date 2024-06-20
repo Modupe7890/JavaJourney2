fc2581a797a6ba4e12e81a0a4f3f958c42f448ac
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-06-20 16:11:41 +0530

    Your message

---
+++ src/Chapter7b/TaskForMapOfElement.java
@@ -0,0 +1,27 @@
+package Chapter7b;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TaskForMapOfElement {
+    public static void main(String[] args) {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("Watermelon:", "Melon");
+        map.put("Banana:", "Fruit");
+        map.put("Cherry:","Fruit");
+        map.put("Pear:", "Fruit");
+        map.put("Cantaloupe:", "Melon");
+        map.put("Blackberry:", "Fruit");
+        map.put("Ginseng:", "Root");
+        map.put("Strawberry:", "Fruit");
+        map.put("Iris:", "Flower");
+        map.put("Potato:", "Tuber");
+
+        for(Map.Entry<String, String> pair : map.entrySet()){
+            String key = pair.getKey();
+            String value = pair.getValue();
+            System.out.println(key+ ":" +value);
+        }
+    }
+}
