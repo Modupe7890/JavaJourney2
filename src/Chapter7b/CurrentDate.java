8036aa28daed9245283cddd40fa6d49e25886c48
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-06-21 22:27:45 +0530

    Your message

---
+++ src/Chapter7b/CurrentDate.java
@@ -0,0 +1,12 @@
+package Chapter7b;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class CurrentDate {
+    public static void main(String[] args) throws IOException {
+        Date today = new Date();
+        System.out.println("Current date: " + today);
+
+    }
+}
