fb6e8b3e659fc746290f96e52634a127391783d2
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-06-24 09:58:06 +0530

    Your message

--- src/Chapter7b/DiffBtwDate.java
+++ src/Chapter7b/DiffBtwDate.java
@@ -1,13 +1,15 @@
-package Chapter7b;
-
-import Chapter3.Date;
-
-public class DiffBtwDate {
-    public static void main(String[] args) {
-
-        Date currentTime = new Date();
-        Thread.sleep(3000);
-        Date newTime = new Date();
-
-    }
-}
+//package Chapter7b;
+//
+//import Chapter3.Date;
+//
+//public class DiffBtwDate {
+//    public static void main(String[] args) {
+//
+//        Date currentTime = new Date();
+//        Thread.sleep(3000);
+//        Date newTime = new Date();
+//
+//        long msDelay = newTime.getDay() - currentTime.getDay();
+//        System.out.println("Time difference is: " + msDelay + " in ms");
+//    }
+//}
---
+++ src/Chapter7b/TimePassed.java
@@ -0,0 +1,16 @@
+package Chapter7b;
+
+import Chapter3.Date;
+
+public class TimePassed {
+    public static void main(String[] args) {
+
+        Date currentTime = new Date();
+        int hours = currentTime.getHours();
+        int minutes = currentTime.getMinutes();
+        int seconds = currentTime.getSeconds();
+
+
+
+    }
+}
