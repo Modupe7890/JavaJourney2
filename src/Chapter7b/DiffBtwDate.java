66c023baccfa7570a7c9fabcc5597c58f6f8d9fb
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-06-24 09:18:03 +0530

    Your message

---
+++ src/Chapter7b/DiffBtwDate.java
@@ -0,0 +1,13 @@
+package Chapter7b;
+
+import Chapter3.Date;
+
+public class DiffBtwDate {
+    public static void main(String[] args) {
+
+        Date currentTime = new Date();
+        Thread.sleep(3000);
+        Date newTime = new Date();
+
+    }
+}
