23d6255938773f3749872c6a292935e9eb6d2245
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-06-24 11:58:09 +0530

    Your message

--- src/Chapter3/Date.java
+++ src/Chapter3/Date.java
@@ -4,6 +4,10 @@ public class Date {
     private int day;
     private int month;
     private int year;
+    private int hour;
+    private int minutes;
+    private int seconds;
+    private int currentTime;
 
     public Date(int day, int month, int year) {
         this.day = day;
@@ -11,17 +15,23 @@ public class Date {
         this.year = year;
     }
 
-        public void setDay(int day){ this.day = day;}
+        public void setDay(int day){
+        this.day = day;
+    }
 
         public int getDay(){
             return day;
         }
-        public void setMonth(int month){this.month = month;}
+        public void setMonth(int month){
+        this.month = month;
+    }
 
         public int getMonth(){
         return month;
         }
-        public void setYear(int year){this.year = year;}
+        public void setYear(int year){
+        this.year = year;
+    }
 
         public int getYear(){
         return year;
@@ -30,6 +40,19 @@ public class Date {
             return month + "/" + day + "/" + year;
 
         }
+        public void setHour(int hour){
+        this.hour = hour;
+        }
 
+    public int getHour() {
+        return hour;
+    }
 
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
 }
