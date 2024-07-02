a405481b289d1365148a776c4839d420d7710cca
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-02 15:58:56 +0530

    Your message

--- src/Chapter8/ThisClass.java
+++ src/Chapter8/ThisClass.java
@@ -6,3 +6,16 @@ public class ThisClass {
         System.out.println(time.buildString());
     }
 }
+// class SimpleTime demonstrates the "this" reference
+    class SimpleTime{
+    private int hour;           // 0 - 23
+    private int minutes;        // 0 - 59
+    private int second;         // 0 - 59
+        // if the constructor uses parameter names identical to
+        // instance variable names the "this" reference is
+        // required to distinguish between the names
+
+    public SimpleTime (int hour, int minutes, int second){
+
+    }
+    }
