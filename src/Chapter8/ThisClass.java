c94932617e3928f54a06e649f18bb90bedd5fdca
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-04 14:27:58 +0530

    Your message

--- src/Chapter8/ThisClass.java
+++ src/Chapter8/ThisClass.java
@@ -2,20 +2,21 @@ package Chapter8;
 // this used implicitly and explicitly to refer to members of an object.
 public class ThisClass {
     public static void main(String[] args) {
-        SimpleTime time = new SimpleTime(15, 30, 19);
+        SimpleTime1 time = new SimpleTime1(15, 30, 19);
         System.out.println(time.buildString());
     }
 }
 // class SimpleTime demonstrates the "this" reference
-    class SimpleTime{
+    class SimpleTime1 {
     private int hour;           // 0 - 23
     private int minutes;        // 0 - 59
     private int second;         // 0 - 59
+
         // if the constructor uses parameter names identical to
         // instance variable names the "this" reference is
         // required to distinguish between the names
 
-    public SimpleTime (int hour, int minutes, int second){
+    public SimpleTime1(int hour, int minutes, int second){
         this.hour = hour;
         this.minutes = minutes;
         this.second = second;
@@ -26,4 +27,7 @@ public class ThisClass {
                             this.toUniversalString(), "toUniversalString()", toUniversalString());
     }
     // convert to String in universal-time format (HH:MM:SS)
+    public String toUniversalString(){
+        return String.format("%02d:%02d:%02d", this.hour, this.minutes, this.second);
+        }
     }
--- src/Chapter8/ThisTest.java
+++ src/Chapter8/ThisTest.java
@@ -2,7 +2,7 @@ package Chapter8;
 // this used implicitly and explicitly to refer to members of an object.
 public class ThisTest {
     public static void main(String[] args) {
-        SimpleTime time = new SimpleTime(15, 30, 19);
+        SimpleTime1 time = new SimpleTime1(15, 30, 19);
         System.out.println(time.buildString());
     }
 }
