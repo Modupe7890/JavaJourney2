f78100c350cca77f6f3641b42f771bf12d65f460
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-07 16:02:05 +0530

    Your message

--- src/Chapter8/ThisClass.java
+++ src/Chapter8/ThisClass.java
@@ -35,5 +35,6 @@ public class ThisClass {
         return String.format("%02d:%02d:%02d", this.hour, this.minutes, this.second);
         return String.format("%02d:%02d:%02d", this.hour, this.minutes, this.second);
         return String.format("%02d:%02d:%02d", this.hour, this.minutes, this.second);
+        return String.format("%02d:%02d:%02d", this.hour, this.minutes, this.second);
         }
     }
