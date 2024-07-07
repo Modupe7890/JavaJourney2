4534ddcb5315b47ab874aeb67e007b39a53d4d13
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-07 18:34:41 +0530

    Your message

---
+++ src/Chapter7b/Factorial.java
@@ -0,0 +1,16 @@
+package Chapter7b;
+
+public class Factorial {
+    public static long getFactorial(final int number){
+            long fact = 1;
+            for(int i = 1; i <= number; i++){
+                fact = fact * i;
+            }
+            return fact;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFactorial(-5));
+        System.out.println(get);
+    }
+}
