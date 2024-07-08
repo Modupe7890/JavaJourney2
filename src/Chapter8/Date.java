2aff16b4aeeb2bbd8548d94889f72689acb7f111
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-08 12:03:01 +0530

    Your message

--- src/Chapter8/Date.java
+++ src/Chapter8/Date.java
@@ -11,7 +11,15 @@ public class Date {
     public Date(int month, int day, int year){
         // check if month in range
         if (month <= 0 || month > 12) {
-            throw new IllegalArgumentException("month (" + month +" must be 1 - 12");
+            throw new IllegalArgumentException("month (" + month +") must be 1 - 12");
+        }
+        // check if day in range for month
+        if(day <= 0 || (day > daysPerMonth[month] && !(month == 2 && day == 29))){
+            throw new IllegalArgumentException("days ("+ day +") out-of-range for the specified month and year");
+        }
+        // check for leap year if month is 2 and day is 29
+        if(month == 2 && day == 29 && !(year % 400 == 0|| (year % 4 == 0 && year % 100 != 0))) {
+            throw new IllegalArgumentException("days (" + day + ") out-of-range for the specified month and year");
         }
     }
 }
