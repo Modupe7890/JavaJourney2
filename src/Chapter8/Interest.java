010bb63e1d5cd17887a7f228f812aafcc3120c1b
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-09 02:09:04 +0530

    Your message

--- src/Chapter8/Interest.java
+++ src/Chapter8/Interest.java
@@ -1,6 +1,7 @@
 package Chapter8;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 
 // Compound-interest calculations with BigDecimal.
 public class Interest {
@@ -10,5 +11,15 @@ public class Interest {
         BigDecimal rate = BigDecimal.valueOf(0.05);
 
         // display headers
+        System.out.printf("%s%20s%n", "Year", "Amount on deposit");
+
+        System.out.printf("%s%20s%n", "Year", "Amount on deposit");
+        for(int year =1; year <= 10; year++){
+            // Calculate the amount for specific year
+            BigDecimal amount = principal.multiply(rate.add(BigDecimal.ONE));
+
+            //Display the year and the month
+            System.out.printf("%4d%20s%n", year, NumberFormat.getCurrencyInstance().format(amount));
+        }
     }
 }
