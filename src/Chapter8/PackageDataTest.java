4201ea7706c8c805ecbdb42b0292547297ea05dc
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-09 01:47:47 +0530

    Your message

---
+++ src/Chapter8/PackageDataTest.java
@@ -0,0 +1,24 @@
+package Chapter8;
+
+public class PackageDataTest {
+    public static void main(String[] args) {
+        PackageData packageData = new PackageData();
+
+        // output String representation of packageData
+        System.out.printf("After instantiation:%n%s%n", packageData);
+
+        // change package access data in packageData object
+        packageData.number = 77;
+        packageData.string = "Goodbye";
+
+        // output String representation of packageData
+        System.out.printf("%nAfter changing values:%n%s%n", packageData);
+    }
+}
+        class PackageData{
+        int number = 0;     // package-access instance variable
+        String string = "Hello";    // package-access instance variable
+
+
+
+        }
