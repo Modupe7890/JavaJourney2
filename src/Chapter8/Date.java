b890fd77a38f501abc20d60f17f393359c478986
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-08 11:37:30 +0530

    Your message

--- src/Chapter8/Date.java
+++ src/Chapter8/Date.java
@@ -6,4 +6,12 @@ public class Date {
     private int year;
 
     private static final int[] daysPerMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    // constructor: confirm proper value for month and day given the year
+    public Date(int month, int day, int year){
+        // check if month in range
+        if (month <= 0 || month > 12) {
+            throw new IllegalArgumentException("month (" + month +" must be 1 - 12");
+        }
+    }
 }
