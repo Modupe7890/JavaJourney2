3ef49281e0a726ec46a406a8c7906b104bf16596
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-08 11:31:29 +0530

    Your message

---
+++ src/Chapter8/Date.java
@@ -0,0 +1,9 @@
+package Chapter8;
+
+public class Date {
+    private int month;
+    private int day;
+    private int year;
+
+    private static final int[] daysPerMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+}
