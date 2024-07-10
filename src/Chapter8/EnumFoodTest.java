b0b6fa81be0ba73981185da7d9644a8a83da4630
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-10 15:42:00 +0530

    Your message

---
+++ src/Chapter8/EnumFoodTest.java
@@ -0,0 +1,12 @@
+package Chapter8;
+
+public class EnumFoodTest {
+    public static void main(String[] args) {
+
+        // Display the enum names and their information
+        for(Food food : Food.values()){
+            System.out.printf("%s: Type = %s, Calories = %d%n",
+                    food.name(), food.getType(), food.getCalories());
+        }
+    }
+}
