d994760626864f67b28decae68f5675af3f64a69
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-10 13:24:31 +0530

    Your message

---
+++ src/Chapter8/RandomNumbers.java
@@ -0,0 +1,13 @@
+package Chapter8;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        SecureRandom random = new SecureRandom();
+
+
+    }
+}
