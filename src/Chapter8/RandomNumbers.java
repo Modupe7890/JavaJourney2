931e3216adec421060201d2da993159927a5a22b
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-10 13:58:51 +0530

    Your message

--- src/Chapter8/RandomNumbers.java
+++ src/Chapter8/RandomNumbers.java
@@ -5,9 +5,15 @@ import java.util.Scanner;
 
 public class RandomNumbers {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         SecureRandom random = new SecureRandom();
 
-
+        System.out.println("Enter a random Number: ");
+        int num = Integer.parseInt(scanner.nextLine());
+        System.out.println("Generated random numbers:");
+        for(int i = 10; i <=100; i++){
+            int randomNumber = 10 + random.nextInt(91);
+            System.out.println(randomNumber);
+        }
     }
 }
