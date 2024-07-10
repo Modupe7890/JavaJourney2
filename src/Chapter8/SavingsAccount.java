5b272388b9bd3edc4b6a695e88616983d1003817
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-10 12:15:44 +0530

    Your message

--- src/Chapter8/SavingsAccount.java
+++ src/Chapter8/SavingsAccount.java
@@ -1,13 +1,15 @@
 package Chapter8;
 
 public class SavingsAccount {
-    private static double annualInterest;
+    private static double annualInterestRate;
     private double savingBalance;
 
     public SavingsAccount(double balance){
         this.savingBalance = balance;
     }
     public void calculateMonthlyInterest (){
-
+        double monthlyInterest = (savingBalance * annualInterestRate) / 12;
+        this.savingBalance += monthlyInterest;
     }
+
 }
