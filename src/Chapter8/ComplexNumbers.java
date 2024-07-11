cd205f1e064ab24c3447d29011d702ba9d50d9f9
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-11 08:18:00 +0530

    Your message

---
+++ src/Chapter8/ComplexNumbers.java
@@ -0,0 +1,11 @@
+package Chapter8;
+
+public class ComplexNumbers {
+    private double realPart;
+    private double imaginaryPart;
+
+    public ComplexNumbers(double realPart, double imaginaryPart){
+        this.realPart = realPart;
+        this.imaginaryPart = imaginaryPart;
+    }
+}
