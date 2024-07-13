57e5dbf91361d69973f7ab17ba24ed5cc6fe4ffa
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-13 07:27:19 +0530

    Your message

--- src/Chapter9/CommissionEmployee.java
+++ src/Chapter9/CommissionEmployee.java
@@ -4,4 +4,9 @@ public class CommissionEmployee extends Object{
     private final String firstName;
     private final String lastName;
     private final String socialSecutityNumber;
+    private final double grossSales;
+    private final double commissionRate;
+
+    // Constructors
+    public CommissionEmployee(String firstName, )
 }
