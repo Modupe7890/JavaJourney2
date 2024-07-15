ec6e54dba3e44a7a9c13fc35e4f8f0bde267b1c1
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-15 10:52:38 +0530

    Your message

--- src/Chapter9/CommissionEmployee.java
+++ src/Chapter9/CommissionEmployee.java
@@ -20,5 +20,9 @@ public class CommissionEmployee extends Object{
         if(commissionRate <= 0.0 || commissionRate >= 1.0){
             throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
         }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+
     }
 }
