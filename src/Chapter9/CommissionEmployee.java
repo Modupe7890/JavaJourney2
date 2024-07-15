e98ce6feb58cfe5f42a03a70645967328a7ed03e
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-15 12:21:59 +0530

    Your message

--- src/Chapter9/CommissionEmployee.java
+++ src/Chapter9/CommissionEmployee.java
@@ -60,4 +60,13 @@ public class CommissionEmployee extends Object{
     public  double earnings(){
         return commissionRate * grossSales;
     }
+    @Override
+    public  String toString(){
+        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f",
+                "commission employee", firstName, lastName,
+                "social security number", socialSecurityNumber,
+                "gross sales", grossSales,
+                "commission rate", commissionRate);
+
+    }
 }
