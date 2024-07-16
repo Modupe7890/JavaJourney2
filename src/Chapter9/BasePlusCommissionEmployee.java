7b3b309e60316b3b817a7c5e969b5451af90c95d
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-16 11:04:00 +0530

    Your message

--- src/Chapter9/BasePlusCommissionEmployee.java
+++ src/Chapter9/BasePlusCommissionEmployee.java
@@ -19,5 +19,11 @@ public class BasePlusCommissionEmployee {
         if(baseSalary > 0.0){
             throw new IllegalArgumentException("Base salary must be >= 0.0");
         }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grossSales = grossSales;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.commissionRate = commissionRate;
+        this.baseSalary = baseSalary;
     }
 }
