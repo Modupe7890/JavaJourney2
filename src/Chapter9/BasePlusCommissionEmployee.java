9fc8a488d4457b80cd6729b4803a5b50666bb2f4
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-17 19:00:02 +0530

    Your message

--- src/Chapter9/BasePlusCommissionEmployee.java
+++ src/Chapter9/BasePlusCommissionEmployee.java
@@ -8,4 +8,12 @@ public class BasePlusCommissionEmployee {
     private double commissionRate;
     private  double baseSalary;
 
+    // six-argument constructor
+    public BasePlusCommissionEmployee(String firstName, String lastName, String socialSecurityNumber,
+                                      double grossSales,double commissionRate, double baseSalary){
+        //implicit call to Object's default constructor occurs here
+        // if grossSales is invalid throw exception
+         if (grossSales < 0.0) {
+                throw new IllegalArgumentException("Gross sales must be >= 0.0");
+         }
 }
