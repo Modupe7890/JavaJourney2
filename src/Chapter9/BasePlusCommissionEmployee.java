8458a384aa7c6e4a54ae0b1305c971e11fa1aabf
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-17 19:14:40 +0530

    Your message

--- src/Chapter9/BasePlusCommissionEmployee.java
+++ src/Chapter9/BasePlusCommissionEmployee.java
@@ -39,4 +39,26 @@ public class BasePlusCommissionEmployee {
     public String getLastName(){
         return lastName;
     }
+    // return social security number
+     public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+     }
+     // set gross sales amount
+     public void setGrossSales(double grossSales) {
+         if (grossSales < 0.0) {
+             throw new IllegalArgumentException("Gross sales must be >= 0.0");
+         }
+         this.grossSales = grossSales;
+     }
+    // return gross sales amount
+     public double getGrossSales() {
+        return grossSales;
+     }
+     // set commission rate
+     public void setCommissionRate(double commissionRate) {
+         if (commissionRate <= 0.0 || commissionRate >= 1.0) {
+             throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+         }
+         this.commissionRate = commissionRate;
+     }
 }
