18fd921868d8c56b349f4532c1716ac31b1e695f
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-17 19:07:40 +0530

    Your message

--- src/Chapter9/BasePlusCommissionEmployee.java
+++ src/Chapter9/BasePlusCommissionEmployee.java
@@ -10,15 +10,26 @@ public class BasePlusCommissionEmployee {
 
     // six-argument constructor
     public BasePlusCommissionEmployee(String firstName, String lastName, String socialSecurityNumber,
-                                      double grossSales,double commissionRate, double baseSalary){
+                                      double grossSales,double commissionRate, double baseSalary) {
         //implicit call to Object's default constructor occurs here
         // if grossSales is invalid throw exception
-         if (grossSales < 0.0) {
-                throw new IllegalArgumentException("Gross sales must be >= 0.0");
-         }
+        if (grossSales < 0.0) {
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+        }
         // if commissionRate is invalid throw exception
         if (commissionRate <= 0.0 || commissionRate >= 1.0) {
-             throw new IllegalArgumentException(
-                     "Commission rate must be > 0.0 and < 1.0");
+            throw new IllegalArgumentException(
+                    "Commission rate must be > 0.0 and < 1.0");
         }
+        // if baseSalary is invalid throw exception
+        if (baseSalary < 0.0) {
+            throw new IllegalArgumentException("Base Salary must be >= 0.0");
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.grossSales = grossSales;
+        this.commissionRate = commissionRate;
+        this.baseSalary = baseSalary;
+    }
 }
