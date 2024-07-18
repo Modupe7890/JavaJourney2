685ff505f8cdd6cd53d9c5a17e8edee9e5d419a2
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-18 10:48:32 +0530

    Your message

--- src/Chapter9/BasePlusCommissionEmployeeTest.java
+++ src/Chapter9/BasePlusCommissionEmployeeTest.java
@@ -3,5 +3,8 @@ package Chapter9;
 public class BasePlusCommissionEmployeeTest {
     public static void main(String[] args) {
         // instantiate BasePlusCommissionEmployee object
+        BasePlusCommissionEmployee employee = new BasePlusCommissionEmployee(
+                        "Bob", "Lewis", "333-33-3333",
+                        5000, .04, 300);
     }
 }
