8509082a94d8b743fb0a243c2b41755030c1bea9
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-18 10:57:44 +0530

    Your message

--- src/Chapter9/BasePlusCommissionEmployeeTest.java
+++ src/Chapter9/BasePlusCommissionEmployeeTest.java
@@ -11,5 +11,7 @@ public class BasePlusCommissionEmployeeTest {
         System.out.printf("%s %s%n", "First name is",employee.getFirstName());
         System.out.printf("%s %s%n", "Last name is",employee.getLastName());
         System.out.printf("%s %s%n", "Social security number is",employee.getSocialSecurityNumber());
+        System.out.printf("%s %.2f%n", "Gross sales is",employee.getGrossSales());
+        System.out.printf("%s %.2f%n", "Commission rate is ", employee.getCommissionRate());
     }
 }
