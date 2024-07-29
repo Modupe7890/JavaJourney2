da17f6470aceaad0d84fa8f10acde81ec890a678
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-07-29 16:21:27 +0530

    Your message

--- src/Chapter9/ReBasePlusCommissionEmployee.java
+++ src/Chapter9/ReBasePlusCommissionEmployee.java
@@ -7,4 +7,9 @@ public class ReBasePlusCommissionEmployee {
     private double grossSales;
     private double commissionRate;
     private double baseSalary;
+
+    public BasePlusCommissionEmployee(String firstName, String lastName, String socialSecurityNumber,
+                                      double grossSales, double commissionRate, double baseSalary) {
+
+    }
 }
