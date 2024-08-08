f88c83e814a0f7d87e5de0565c785b07d20f6ccb
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-08 10:56:03 +0530

    Your message

---
+++ src/Chapter9/Orange.java
@@ -0,0 +1,16 @@
+package Chapter9;
+
+public class Orange extends Fruit {
+    public Orange(String shape, String color, int calories) {
+
+
+        @Override
+                public String toString; {
+            return "Orange{" +
+                    "shape='" + getShape() + '\'' +
+                    ", color='" + getColor() + '\'' +
+                    ", calories=" + getCalories() +
+                    '}';
+        }
+    }
+}
