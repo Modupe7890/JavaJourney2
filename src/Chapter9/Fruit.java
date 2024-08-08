4d9bc192af1610f1482ec55625b8274297a12e70
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-08 11:06:05 +0530

    Your message

---
+++ src/Chapter9/Fruit.java
@@ -0,0 +1,13 @@
+package Chapter9;
+
+public class Fruit {
+    private String shape;
+    private String colour;
+    private  int calories;
+
+    public Fruit( String shape, String colour, int calories){
+        this.shape = shape;
+        this.colour = colour;
+        this.calories = calories;
+    }
+}
