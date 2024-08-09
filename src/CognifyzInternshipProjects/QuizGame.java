f036540e08818cf9638047ebdf73dca946398c12
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-09 18:36:20 +0530

    Your message

--- src/CognifyzInternshipProjects/QuizGame.java
+++ src/CognifyzInternshipProjects/QuizGame.java
@@ -9,7 +9,9 @@ public class QuizGame {
 
         //quiz question
         System.out.println("1.) What is the smallest prime number?");
-        System.out.println("2.) Who wrote 'Romeo and Juliet'?");
+        System.out.println("2.) How many time zones are there in Russia? (11)");
         System.out.println("3.) What is the capital of France?");
+        System.out.println("4.) How many days does it take for the Earth to orbit the Sun? (365)");
+        System.out.println("5.) How many stripes are there on the US flag? (13)");
     }
 }
