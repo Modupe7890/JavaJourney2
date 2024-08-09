5736fb15a301c9b88b16d8546f74ef1fbf48562b
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-09 19:22:04 +0530

    Your message

--- src/CognifyzInternshipProjects/QuizGame.java
+++ src/CognifyzInternshipProjects/QuizGame.java
@@ -12,15 +12,17 @@ public class QuizGame {
         System.out.println("2.) How many time zones are there in Russia?");
         System.out.println("3.) What is the capital of France?");
         System.out.println("4.) How many days does it take for the Earth to orbit the Sun?");
-        System.out.println("5.) How many stripes are there on the US flag? (13)");
+        System.out.println("5.) How many stripes are there on the US flag?");
 
         // Array of corresponding answers to the quiz questions
         String[][] options = {
                 {"a) 0", "b) 1", "c) 2", "d) 3"},
                 {"a) 16", "b) 8", "c) 11", "d) 4"},
-                {"a) Earth", "b) Mars", "c) Jupiter", "d) Saturn"},
+                {"a) Paris", "b) London", "c) Rome", "d) Berlin"},
                 {"a) 365", "b) 367", "c) 366", "d) 400"},
                 {"a) 23", "b) 13", "c) 12", "d) 10"},
         };
+        //Array of correct answers
+        char[] answers = {'c', 'c', 'a', 'a', 'b'};
     }
 }
