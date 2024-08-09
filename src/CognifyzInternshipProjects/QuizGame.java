d448b63f63d4bcc630e778a291a568de44977b6c
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-09 17:48:23 +0530

    Your message

--- src/CognifyzInternshipProjects/QuizGame.java
+++ src/CognifyzInternshipProjects/QuizGame.java
@@ -7,5 +7,9 @@ public class QuizGame {
 
         Scanner scanner = new Scanner(System.in);
 
+        //quiz question
+        System.out.println("1.) What is the smallest prime number?");
+        System.out.println("2.) Who wrote 'Romeo and Juliet'?");
+        System.out.println("3.) What is the capital of France?");
     }
 }
