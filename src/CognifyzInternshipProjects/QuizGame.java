c5299f7b7635ce611cb473b93effa88bd91cbf16
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-09 18:54:34 +0530

    Your message

--- src/CognifyzInternshipProjects/QuizGame.java
+++ src/CognifyzInternshipProjects/QuizGame.java
@@ -11,7 +11,7 @@ public class QuizGame {
         System.out.println("1.) What is the smallest prime number?");
         System.out.println("2.) How many time zones are there in Russia?");
         System.out.println("3.) What is the capital of France?");
-        System.out.println("4.) How many days does it take for the Earth to orbit the Sun? (365)");
+        System.out.println("4.) How many days does it take for the Earth to orbit the Sun?");
         System.out.println("5.) How many stripes are there on the US flag? (13)");
 
         // Array of corresponding answers to the quiz questions
@@ -19,8 +19,8 @@ public class QuizGame {
                 {"a) 0", "b) 1", "c) 2", "d) 3"},
                 {"a) 16", "b) 8", "c) 11", "d) 4"},
                 {"a) Earth", "b) Mars", "c) Jupiter", "d) Saturn"},
-                {"a) 365", "b) 367", "c) 366", "d) 400"}
-
+                {"a) 365", "b) 367", "c) 366", "d) 400"},
+                {"a) 23", "b) 13", "c) 12", "d) 10"},
         };
     }
 }
