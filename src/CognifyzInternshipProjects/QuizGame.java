754239618b836f369708abc86ca505d2f5ce8ea9
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-09 17:22:57 +0530

    Your message

--- src/CognifyzInternshipProjects/QuizGame.java
+++ src/CognifyzInternshipProjects/QuizGame.java
@@ -1,4 +1,11 @@
 package CognifyzInternshipProjects;
 
+import java.util.Scanner;
+
 public class QuizGame {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
 }
