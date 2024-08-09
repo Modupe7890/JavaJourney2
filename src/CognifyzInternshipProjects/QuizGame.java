e28dd70f4df9d1f3790923f596b1b52e5255ba8a
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-09 20:02:09 +0530

    Your message

--- src/CognifyzInternshipProjects/QuizGame.java
+++ src/CognifyzInternshipProjects/QuizGame.java
@@ -8,11 +8,13 @@ public class QuizGame {
         Scanner scanner = new Scanner(System.in);
 
         //quiz questions
-        System.out.println("1.) What is the smallest prime number?");
-        System.out.println("2.) How many time zones are there in Russia?");
-        System.out.println("3.) What is the capital of France?");
-        System.out.println("4.) How many days does it take for the Earth to orbit the Sun?");
-        System.out.println("5.) How many stripes are there on the US flag?");
+        String[] questions = {
+                "1.) What is the smallest prime number?",
+                "2.) How many time zones are there in Russia?",
+                "3.) What is the capital of France?",
+                "4.) How many days does it take for the Earth to orbit the Sun?",
+                "5.) How many stripes are there on the US flag?",
+        };
 
         // Array of corresponding answers to the quiz questions
         String[][] options = {
@@ -26,13 +28,13 @@ public class QuizGame {
         char[] answers = {'c', 'c', 'a', 'a', 'b'};
 
         int score = 0;
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(i);
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
             for (String option : options[i]) {
                 System.out.println(option);
             }
             System.out.print("Choose the correct option (a/b/c/d): ");
-            char userAnswer = scanner.next().toLowerCase().charAt(0);
+            char userAnswer = scanner.next().toLowerCase().charAt(5);
 
             if (userAnswer == answers[i]) {
                 System.out.println("Correct!\n");
@@ -41,6 +43,6 @@ public class QuizGame {
                 System.out.println("Wrong! The correct answer was " + answers[i] + ".\n");
             }
         }
-        System.out.println("Quiz Over! Your final score is: " + score + "/" + args.length);
+        System.out.println("Quiz Over! Your final score is: " + score + "/" + questions.length);
     }
 }
