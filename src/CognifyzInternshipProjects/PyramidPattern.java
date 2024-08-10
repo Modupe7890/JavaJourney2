02fd8dd7fa2430432fd7794f8bbf6c02413440d0
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-10 20:29:04 +0530

    Your message

--- src/CognifyzInternshipProjects/PyramidPattern.java
+++ src/CognifyzInternshipProjects/PyramidPattern.java
@@ -2,16 +2,20 @@ package CognifyzInternshipProjects;
 
 public class PyramidPattern {
     public static void main(String[] args) {
-        // number of rows
+        // number of pattern rows
         int number = 4;
-        for(int i = 1; i <= number; i++){
+        for(int i = 0; i <= number; i++){
             for (int j = number - 1; j > 0; j--){
-                System.out.println("   ");
+                if(j <= i){
+                    System.out.println(j);
+                }else {
+                    System.out.println(" ");
+                }
             }
-            for(int k = 1; k <= number; k++){
-                System.out.println("  ");
+            for(int k = 2; k <= number; k++){
+                System.out.println(k);
             }
-            System.out.println();
+            System.out.println(" ");
         }
     }
 }
