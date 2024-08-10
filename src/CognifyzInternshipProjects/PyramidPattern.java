f7798529d81c07a9e1f764ed7552b6209b043010
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-10 13:27:50 +0530

    Your message

--- src/CognifyzInternshipProjects/PyramidPattern.java
+++ src/CognifyzInternshipProjects/PyramidPattern.java
@@ -2,6 +2,10 @@ package CognifyzInternshipProjects;
 
 public class PyramidPattern {
     public static void main(String[] args) {
+        // number of rows
+        int number = 4;
+        for(int i = 0; i <= number; i++){
 
+        }
     }
 }
