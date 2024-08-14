a259e4405c5dd80c74b901329dd17336848ee222
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-14 16:21:20 +0530

    Your message

---
+++ src/CognifyzInternshipProjects/TemperatureConverter.java
@@ -0,0 +1,18 @@
+package CognifyzInternshipProjects;
+
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please kindly read through and follow the instructions.");
+        System.out.println("-------------------------------------------------------");
+
+        System.out.print("Enter your Temperature: ");
+        double temp = scanner.nextDouble();
+
+        System.out.println();
+    }
+}
