d36a4ae0637cb9fc940b1021e65d98fdd4e9707d
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-15 11:08:09 +0530

    Your message

--- src/CognifyzInternshipProjects/TemperatureConverter.java
+++ src/CognifyzInternshipProjects/TemperatureConverter.java
@@ -31,11 +31,6 @@ public class TemperatureConverter {
                 break;
             default:
                 System.out.println("Invalid Option");
-                System.out.println("Invalid Option");
-                System.out.println("Invalid Option");
-                System.out.println("Invalid Option");
-                System.out.println("Invalid Option");
-                System.out.println("Invalid Option");
         }
     }
     public static double fahrenheitToCelsius (double fahrenheit){
