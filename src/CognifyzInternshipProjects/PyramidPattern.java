ad6f62d9f5f81319e9c7e3696449998df864dfef
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-18 18:35:03 +0530

    Your message

--- src/CognifyzInternshipProjects/PyramidPattern.java
+++ src/CognifyzInternshipProjects/PyramidPattern.java
@@ -22,6 +22,7 @@ public class PyramidPattern {
             System.out.println();
             System.out.println();
             System.out.println();
+            System.out.println();
         }
     }
 }
