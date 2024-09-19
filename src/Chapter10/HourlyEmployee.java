9413bc8e9711d916f7a262b0c5caf864ac3706f1
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-09-19 17:35:36 +0530

    Your message

--- src/Chapter10/HourlyEmployee.java
+++ src/Chapter10/HourlyEmployee.java
@@ -1,6 +1,6 @@
 package Chapter10;
 
-public class HourlyEmployee extends Employee{
+public class HourlyEmployee extends Employee {
     private double wage;
     private double hours;
 
@@ -18,29 +18,33 @@ public class HourlyEmployee extends Employee{
         this.hours = hours;
         this.wage = wage;
     }
+
     // set wage
-        public void setWage(double wage) {
-            if (wage < 0.0) { // validate wage
-                throw new IllegalArgumentException("Hourly wage must be >= 0.0");
-            }
-            this.wage = wage;
+    public void setWage(double wage) {
+        if (wage < 0.0) { // validate wage
+            throw new IllegalArgumentException("Hourly wage must be >= 0.0");
         }
-        // get wage
+        this.wage = wage;
+    }
+
+    // get wage
     public double getWage() {
         return wage;
     }
+
     // set hours worked
-        public void setHours(double hours) {
-            if ((hours < 0.0) || (hours > 168.0)) { // validate hours
-                throw new IllegalArgumentException("Hours worked must be >= 0.0 and <= 168.0");
-            }
-            this.hours = hours;
+    public void setHours(double hours) {
+        if ((hours < 0.0) || (hours > 168.0)) { // validate hours
+            throw new IllegalArgumentException("Hours worked must be >= 0.0 and <= 168.0");
         }
-        // return hours worked
+        this.hours = hours;
+    }
+    // return hours worked
 
     public double getHours() {
         return hours;
     }
+
     // calculate earnings; override abstract method earnings in Employee
     @Override
     public double earnings() {
@@ -50,4 +54,12 @@ public class HourlyEmployee extends Employee{
             return 40 * getWage() + (getHours() - 40) * getWage() * 1.5;
         }
     }
-}
+
+    // return String representation of HourlyEmployee object
+    @Override
+    public String toString() {
+        return String.format("hourly employee: %s%n%s: $%,.2f; %s: %,.2f",
+                super.toString(), "hourly wage", getWage(),
+                "hours worked", getHours());
+        }
+    }
