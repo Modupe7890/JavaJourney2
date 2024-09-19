57b290027a01364da9f517dfcc05adc5d3160cdd
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-09-19 17:33:14 +0530

    Your message

--- src/Chapter10/HourlyEmployee.java
+++ src/Chapter10/HourlyEmployee.java
@@ -34,5 +34,20 @@ public class HourlyEmployee extends Employee{
             if ((hours < 0.0) || (hours > 168.0)) { // validate hours
                 throw new IllegalArgumentException("Hours worked must be >= 0.0 and <= 168.0");
             }
+            this.hours = hours;
         }
+        // return hours worked
+
+    public double getHours() {
+        return hours;
+    }
+    // calculate earnings; override abstract method earnings in Employee
+    @Override
+    public double earnings() {
+        if (getHours() <= 40) { // no overtime
+            return getWage() * getHours();
+        } else {
+            return 40 * getWage() + (getHours() - 40) * getWage() * 1.5;
+        }
+    }
 }
