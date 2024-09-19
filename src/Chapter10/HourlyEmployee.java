40aeaea19e4820b6c3b485d0af3669c9dca29bea
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-09-19 16:31:13 +0530

    Your message

--- src/Chapter10/HourlyEmployee.java
+++ src/Chapter10/HourlyEmployee.java
@@ -8,5 +8,9 @@ public class HourlyEmployee extends Employee{
     public HourlyEmployee(String firstName, String lastName, String socialSecurityNumber,
                           double wage, double hours) {
         super(firstName, lastName, socialSecurityNumber);
+
+        if (wage < 0.0) { // validate wage
+            throw new IllegalArgumentException("Hourly wage must be >= 0.0");
+        }
     }
 }
