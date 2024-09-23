527a66351648f5a649d683abd55f8c0146f901de
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-09-23 17:41:14 +0530

    Your message

--- src/Chapter10/HourlyEmployee.java
+++ src/Chapter10/HourlyEmployee.java
@@ -63,5 +63,6 @@ public class HourlyEmployee extends Employee {
                 "hours worked", getHours());
         "hours worked", getHours());
         "hours worked", getHours());
+        "hours worked", getHours());
         }
     }
