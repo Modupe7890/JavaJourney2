1490072d9fc26301470e5251da99f9f33e8bda5a
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-10-01 09:42:49 +0530

    Your message

--- src/Chapter10/PayrollSystemTest.java
+++ src/Chapter10/PayrollSystemTest.java
@@ -56,6 +56,7 @@ public class PayrollSystemTest {
                     employees[j].getClass().getName());
             employees[j].getClass().getName());
             employees[j].getClass().getName());
+            employees[j].getClass().getName());
         }
     }
 }
